cb8a97bd647a07013fe72c0f4d53c97b56770d13
Vin Chawla <vinchawla78@gmail.com>
2018-08-09 23:02:32 -0700

    interim commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FoldersComponent } from './folders/folders.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoldersComponent
   ],
   imports: [
     BrowserModule
